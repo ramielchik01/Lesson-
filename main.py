bb8ebd623bdaa501d071db78f68ed898c0a319ee
ramielchik01 <ramiel_0101@icloud.com>
2026-04-26 05:39:16 +0000

    Apr 25, 2026, 10:39 PM

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print('hello')
+class universities:
+    def __init__(self, fillial):
+        self.fillial = fillial
+    
+    def nameOfFillials(self):
+        return f'there are names: {self.fillial}'
+univer1 = ("MGU")
+print(univer1.fillial)
