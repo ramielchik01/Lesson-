bdfb551f0342984af1ee9c78498464738b46157d
ramielchik01 <ramiel_0101@icloud.com>
2026-04-26 06:09:55 +0000

    Apr 25, 2026, 11:09 PM

--- main.py
+++ main.py
@@ -7,3 +7,21 @@ class Universities:
 
 univer1 = Universities("MGU")
 print(univer1.nameOfFillials())
+# bank
+class Bank:
+    def __init__(self, cardNumber, cash):
+        self.cardNumber = cardNumber
+        self.__cash = cash      
+    def user(self):
+        return f'card number is {self.cardNumber} and cash is {self.cash}' 
+    def check(self):
+        if self.cash >= 0:
+            print(f'your cash is {self.cash} you can buy everything')
+        else:
+            print("you do not have enough money")
+user1bank = Bank(3533465235, 1000)
+user2bank = Bank(456435765243, -10)
+print(user1bank.user())
+print(user1bank.check())
+print(user2bank.user())
+print(user2bank.check())
